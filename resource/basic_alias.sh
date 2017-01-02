de61016a957423b8299e17541cfee1d5081de386
Bruno Fernando dos Santos Silva <bruno2fernando@gmail.com>
2017-01-01 22:15:29 -0300

    Improve alias vrl

--- resource/basic_alias.sh
+++ resource/basic_alias.sh
@@ -2,8 +2,8 @@
 alias hostedit='current_path=$(pwd) && atom /etc/hosts && cd $current_path' # Alias to edit the Hosts file
 
 alias vup='current_path=$(pwd) && ~/Homestead && vagrant up && cd $current_path' # Alias to Vagrant up
-alias vrl='current_path=$(pwd) && ~/Homestead && vagrant reload --provision && cd $current_path'# Alias to Vagrant reload
-alias vhalt='current_path=$(pwd) && ~/Homestead && vagrant halt && cd $current_path'# Alias to Vagrant halt
+alias vrl='current_path=$(pwd) && ~/Homestead && vagrant reload --provision && cd $current_path' # Alias to Vagrant reload
+alias vhalt='current_path=$(pwd) && ~/Homestead && vagrant halt && cd $current_path' # Alias to Vagrant halt
 alias vedit='current_path=$(pwd) && atom ~/.homestead/Homestead.yaml && cd $current_path' # Alias to edit the Homestead.yaml file
 
 alias zshedit='current_path=$(pwd) && atom ~/.zshrc && cd $current_path' # Alias to edit the .zshrc file
