26328bc01fdb98fe2a30bcd8b042edd16dbb9ca6
Bruno Fernando dos Santos Silva <bruno2fernando@gmail.com>
2017-01-19 13:38:47 -0300

    Add ssh

--- resource/basic_alias.sh
+++ resource/basic_alias.sh
@@ -1,6 +1,6 @@
-
 alias hostedit='current_path=$(pwd) && atom /etc/hosts && cd $current_path' # Alias to edit the Hosts file
 
+alias vssh='current_path=$(pwd) && ~/Homestead && vagrant ssh && cd $current_path' # Alias to Vagrant ssh
 alias vup='current_path=$(pwd) && ~/Homestead && vagrant up && cd $current_path' # Alias to Vagrant up
 alias vrl='current_path=$(pwd) && ~/Homestead && vagrant reload --provision && cd $current_path' # Alias to Vagrant reload
 alias vhalt='current_path=$(pwd) && ~/Homestead && vagrant halt && cd $current_path' # Alias to Vagrant halt
