1d0b844709a942f042deefb693f29fce4e7394b2
Bruno Fernando dos Santos Silva <bruno2fernando@gmail.com>
2018-07-10 10:07:09 -0300

    Update basic_alias.sh

--- resource/basic_alias.sh
+++ resource/basic_alias.sh
@@ -1,9 +1,36 @@
-alias hostedit='current_path=$(pwd) && atom /etc/hosts && cd $current_path' # Alias to edit the Hosts file
+export PATH_HOMESTEAD=~/Homestead
 
-alias vssh='current_path=$(pwd) && ~/Homestead && vagrant ssh && cd $current_path' # Alias to Vagrant ssh
-alias vup='current_path=$(pwd) && ~/Homestead && vagrant up && cd $current_path' # Alias to Vagrant up
-alias vrl='current_path=$(pwd) && ~/Homestead && vagrant reload --provision && cd $current_path' # Alias to Vagrant reload
-alias vhalt='current_path=$(pwd) && ~/Homestead && vagrant halt && cd $current_path' # Alias to Vagrant halt
-alias vedit='current_path=$(pwd) && atom ~/.homestead/Homestead.yaml && cd $current_path' # Alias to edit the Homestead.yaml file
+# Opens the Mac hosts file
+alias hostedit='f(){
+  echo "Opening host file"
+  if [ "$1" != "" ]
+  then
+    current_path=$(pwd) && $1 /etc/hosts && cd $current_path
+  else
+    current_path=$(pwd) && atom /etc/hosts && cd $current_path
+  fi};f'
 
-alias zshedit='current_path=$(pwd) && atom ~/.zshrc && cd $current_path' # Alias to edit the .zshrc file
+# Open the zsh file 
+alias zshedit='f(){
+  echo "Opening .zsh file"
+  if [ "$1" != "" ]
+  then
+    current_path=$(pwd) && $1 ~/.zshrc && cd $current_path
+  else
+    current_path=$(pwd) && atom ~/.zshrc && cd $current_path
+  fi};f'
+
+# Open the Homestead.yaml file
+alias vedit='f(){
+  echo "Opening Homestead.yaml file"
+  if [ "$1" != "" ]
+  then
+    current_path=$(pwd) && $1 $PATH_HOMESTEAD/Homestead.yaml && cd $current_path
+  else
+    current_path=$(pwd) && atom $PATH_HOMESTEAD/Homestead.yaml && cd $current_path
+  fi};f'
+
+alias vssh='current_path=$(pwd) && $PATH_HOMESTEAD && vagrant ssh && cd $current_path' # Alias to Vagrant ssh
+alias vup='current_path=$(pwd) && $PATH_HOMESTEAD && vagrant up && cd $current_path' # Alias to Vagrant up
+alias vrl='current_path=$(pwd) && $PATH_HOMESTEAD && vagrant reload --provision && cd $current_path' # Alias to Vagrant reload
+alias vhalt='current_path=$(pwd) && $PATH_HOMESTEAD && vagrant halt && cd $current_path' # Alias to Vagrant halt
