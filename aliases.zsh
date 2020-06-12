61199f78c42a3c31788d1f50a0cfa30c559e530d
Bruno Fernando dos Santos Silva <bruno2fernando@gmail.com>
2020-06-12 02:08:53 -0300

    FEAT: Alias zsh
    Update Alias Edit aliases.zsh, Edit .zshrc and Reload .zshrc

--- aliases.zsh
+++ aliases.zsh
@@ -1,3 +1,7 @@
 alias cls="clear"
-alias zreload=". ~/.zshrc && echo 'ZSH config reloaded from ~/.zshrc'"
+
+alias zshalias="code $HOME/.oh-my-zsh/custom/aliases.zsh"
+alias zshconfig="code $HOME/.zshrc"
+alias reload=". $HOME/.zshrc"
+
 alias raw2jpg="time_stamp=$(date +%Y-%m-%d-%T) && folder_name="JPEG_RAW_2_JPG/${time_stamp}" && mkdir -p "${folder_name}" && for i in *.CR2; do sips -s format jpeg $i --out "${folder_name}/${i%.*}.jpg"; done"
